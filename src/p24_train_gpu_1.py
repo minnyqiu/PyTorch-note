4598e1fbdceb4815fe10f73f7a375f69e2507e58
a100-server <ballmaxwell@163.com>
2024-12-11 19:29:16 +0800

    add GPU train and compare with CPU in training time

--- src/p24_train_gpu_1.py
+++ src/p24_train_gpu_1.py
@@ -1,10 +1,7 @@
-from turtle import forward
-from scipy import optimize
 import torch
 import torchvision
 from torch.utils.data import DataLoader
 from torch import nn
-from p23_model import *
 from torch.utils.tensorboard import SummaryWriter
 import time
 
@@ -25,11 +22,34 @@ print(f'test data length:{test_data_size}')
 train_dataloader = DataLoader(train_data, batch_size=64)
 test_dataloader = DataLoader(test_data, batch_size=64)
 
+# 搭建神经网络
+class Tudui(nn.Module):
+    def __init__(self):
+        super(Tudui, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 32, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 5, 1, 2),
+            nn.MaxPool2d(2),
+            nn.Flatten(),
+            nn.Linear(64*4*4, 64),
+            nn.Linear(64,10)
+        )
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
 # 创建网络模型
 tudui = Tudui()
+if torch.cuda.is_available():
+    tudui = tudui.cuda() # 可用再转移到cuda
 
 # 损失函数
 loss_fn = nn.CrossEntropyLoss()
+if torch.cuda.is_available():
+    loss_fn = loss_fn.cuda() # 转移到cuda
 
 # 优化器
 # 1e-2 = 1 * (10)^(-2)
@@ -48,7 +68,7 @@ total_test_step = 0
 epoch = 10
 
 # 添加tensorboard
-writer = SummaryWriter('/home/data/minyu/PyTorch-note/logs/p23')
+writer = SummaryWriter('/home/data/minyu/PyTorch-note/logs/p24')
 
 start_time = time.time() # 记录开始时间
 
@@ -59,6 +79,10 @@ for i in range(epoch):
     tudui.train()  # 作用? BatchNorm module 
     for data in train_dataloader:
         imgs, targets = data
+        if torch.cuda.is_available():
+            imgs = imgs.cuda() # 转移到cuda
+        if torch.cuda.is_available():
+            targets = targets.cuda() # 转移到cuda
         outputs = tudui(imgs)
         loss = loss_fn(outputs, targets)
 
@@ -70,10 +94,11 @@ for i in range(epoch):
         total_train_step = total_train_step + 1 # 每次都打印
         # 逢百打印
         if total_train_step % 100 == 0:
-            end_time = time.time() # 记录100次的结束时间
-            print(end_time - start_time)
+            end_time = time.time() # 从训练开始到当前完成 100 次训练步骤所用的时间
+            print(f'100次训练耗时:{end_time - start_time:.2f}秒')
             print(f'训练次数: {total_train_step}, Loss: {loss.item()}')
             writer.add_scalar('train_loss', loss.item(), total_train_step) 
+            start_time = end_time  # 更新开始时间 计算每100次所用的时间
 
     # 测试步骤开始
     tudui.eval()  # 作用? BatchNorm module 
@@ -82,6 +107,10 @@ for i in range(epoch):
     with torch.no_grad(): # 不要梯度
         for data in test_dataloader:
             imgs, targets = data
+            if torch.cuda.is_available():
+                imgs = imgs.cuda() # 转移到cuda
+            if torch.cuda.is_available():
+                targets = targets.cuda() # 转移到cuda
             outputs = tudui(imgs)
             loss = loss_fn(outputs, targets) # loss是tensor类型
             total_test_loss = total_test_loss + loss.item() # 用.item()变成数字
@@ -98,7 +127,7 @@ for i in range(epoch):
     total_test_step = total_test_step + 1
 
     # 保存模型
-    torch.save(tudui, f'PyTorch-note/output/tudui_{i}.pth')
+    torch.save(tudui, f'PyTorch-note/output/tudui_{i}_gpu.pth')
     print('模型已保存')
 
 writer.close()
@@ -108,30 +137,13 @@ writer.close()
 
 
 """ 
-用正确率衡量分类问题
+1.网络模型 tudui
 
-2 x input 
-Model (2分类)
+2.数据 (输入, 标注) imgs, targets = data
 
-输出的形式
-outputs = 
-[0.1, 0.2], [第0类的概率 第1类的概率]
-[0.3, 0.4] 
+3.损失函数 loss_fn
 
-@ 转换成targets
-0       1
-
-用argmax求出横向最大值所在的位置
-
-preds = 
-[1]
-[1]
-
-inputs target = 
-[0]
-[1]   
-
-preds == inputs target 判断预测结果
-[false, true].sum() = 1 # 求出正确位置的个数
+对以上三个调用.cuda() 再返回
 
 """
+
